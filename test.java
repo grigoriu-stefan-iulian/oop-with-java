5abccb7126047609aacd7faa86f65f2574b5f9d6
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-18 14:14:44 +0200

    learn java and OOP

---
+++ test.java
@@ -0,0 +1,23 @@
+public class Animal {
+    private String name;
+    private int weight;
+
+}
+
+abstract public class Creature {
+    protected String name;
+    protected int weight;
+    protected String sound;
+
+    public abstract void setName(String newName);
+    public abstract void setWeight(double newWeight)
+}
+
+//Strategy Pattern
+   // When you want to define a class that will have one behavior that is similar to other behaviors in a list
+   // when you need to use one of several behaviors dinamically
+   // Reduce long list of conditionals
+   // Avoid duplicate code
+   // Keeps class changes from forcing other class changes
+   // Can hide complicated / secret code from the user
+   // Negative: Increased number of Objects / Classes 
